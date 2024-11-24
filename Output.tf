14d963195eef529c7ab9e5eb3b56204abb0e525b
Optimus <louismariae78@gmail.com>
2024-11-23 17:19:52 -0800

    New Commit

---
+++ Output.tf
@@ -0,0 +1,6 @@
+output "Username" {
+  value = aws_lightsail_instance.Marufu.username
+}
+output "Public_IP" {
+  value = aws_lightsail_instance.Marufu.public_ip_address
+}
---
+++ Version.tf
@@ -0,0 +1,12 @@
+# Create a new GitLab Lightsail Instance
+resource "aws_lightsail_instance" "Marufu" {
+  name              = "Optimus"
+  availability_zone = "us-east-1a"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "nano_3_0"
+  user_data = file("Data.sh")
+  tags = {
+    jack = "DevOps"
+    team = "Cloud_Team"
+  }
+}
